72bfa51cfff796d51dcd74441fa5c13e8aa738ed
Jonathan Cai <caimjonathan@gmail.com>
2021-11-11 16:48:54 -0500

    feat: /api/opensea/[address] get assets call

---
+++ pages/api/opensea/[address].ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+/**
+ * Fetches the assets of the given address using the Opensea API.
+ * https://api.opensea.io/api/v1/assets
+ */
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { address: ownerAddress } = req.query
+  const resp = await fetch(
+    `https://api.opensea.io/api/v1/assets?owner=${ownerAddress}&order_direction=desc&offset=0&limit=20`,
+  )
+  res.status(200).json(await resp.json())
+}

4a18a90f9d684398487c9bb6f824aaf43e6a4ed0
Jonathan Cai <caimjonathan@gmail.com>
2021-11-11 19:09:45 -0500

    feat: middle ellipses addresses and page styling

---
+++ lib/util.ts
@@ -0,0 +1,14 @@
+/**
+ * Inserts middle ellipses into a given string
+ * @param str The string to insert ellipses into
+ * @param cutoffDecimals The minimum number of chars in string to insert ellipses
+ * @param begginingDecimals The number of decimals to begin the result
+ * @param endingDecimals The number of decimals to end the result
+ * @returns
+ */
+export function middleEllipses(str: string, cutoffDecimals: number, begginingDecimals: number, endingDecimals: number) {
+  if (str.length > cutoffDecimals) {
+    return str.substr(0, begginingDecimals) + '...' + str.substr(str.length - endingDecimals, str.length)
+  }
+  return str
+}
--- pages/[address].tsx
+++ pages/[address].tsx
@@ -4,9 +4,9 @@ import Head from 'next/head'
 import { Toaster } from 'react-hot-toast'
 
 import Navbar from '../components/navbar'
+import { middleEllipses } from '../lib/util'
 
 const AddressPage: NextPage = ({ assetsByCollection, address }: any) => {
-  console.log({ assetsByCollection })
   return (
     <div className="max-w-screen-xl m-auto pb-4 md:pb-12">
       <Head>
@@ -17,19 +17,23 @@ const AddressPage: NextPage = ({ assetsByCollection, address }: any) => {
       <Toaster />
       <Navbar />
       <div className="flex flex-col space-y-4">
-        <h4 className="text-lg ">{Object.keys(assetsByCollection).length} collections.</h4>
-        <h4 className="text-lg ">Address: {address}.</h4>
+        <h4 className="text-lg ">Address: {middleEllipses(address, 4, 6, 4)}</h4>
+        <h4 className="text-lg ">{Object.keys(assetsByCollection).length} collections</h4>
       </div>
       <div className="flex flex-col space-x-2 flex-wrap space-y-2 mt-8 ">
         {Object.keys(assetsByCollection).map((collectionSlug: any, i: number) => {
           const collection = assetsByCollection[collectionSlug]
           return (
-            <div key={i} className="flex space-x-4">
-              <img src={collection.image_url} width="40" height="40" />
-              <span>{collection.assets.length}x</span>
-              <span>{collection.name}</span>
-              <span>{collection.stats.floor_price}Ξ</span>
-              <span>{(collection.assets.length * collection.stats.floor_price).toFixed(3)}Ξ</span>
+            <div key={i} className="flex space-x-4 justify-between">
+              <div className="flex space-x-4">
+                <img src={collection.image_url} width="40" height="40" />
+                <span>{collection.assets.length}x</span>
+                <span>{collection.name}</span>
+              </div>
+              <div className="flex space-x-4">
+                <span>{collection.stats.floor_price}Ξ</span>
+                <span>{(collection.assets.length * collection.stats.floor_price).toFixed(3)}Ξ</span>
+              </div>
             </div>
           )
         })}

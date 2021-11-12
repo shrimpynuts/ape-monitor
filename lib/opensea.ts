69b9594e24066465ae657fd5a56c7e61a7e6fb53
Jonathan Cai <caimjonathan@gmail.com>
2021-11-12 00:01:55 -0500

    feat: fetch collections for owner, and fix flex list styling

---
+++ lib/opensea.ts
@@ -0,0 +1,44 @@
+export const getCollectionsForOwner = async (ownerAddress: string) => {
+  const resp = await fetch(`https://api.opensea.io/api/v1/collections?asset_owner=${ownerAddress}&offset=0&limit=300`)
+  const collections = await resp.json()
+  return collections
+}
+
+export const getAssetsForOwner = async (ownerAddress: string) => {
+  const resp = await fetch(
+    `https://api.opensea.io/api/v1/assets?owner=${ownerAddress}&order_direction=desc&offset=0&limit=50`,
+  )
+  const { assets } = await resp.json()
+  return assets
+}
+
+export const getCollectionStats = async (slug: string) => {
+  const resp = await fetch(`https://api.opensea.io/api/v1/collection/${slug}/stats`)
+  const { stats } = await resp.json()
+  return stats
+}
+
+export const getAssetsGroupedByCollectionForOwner = async (ownerAddress: string) => {
+  // Get all assets for the given address
+  const assets = await getAssetsForOwner(ownerAddress)
+
+  // Group all assets by collection slug
+  const byCollection = assets.reduce((acc: any, asset: any) => {
+    const slug = asset.collection.slug
+    console.log({ slug })
+    if (acc[slug]) {
+      acc[slug].assets.push(asset)
+    } else {
+      acc[slug] = { ...asset.collection, assets: [asset] }
+    }
+    return acc
+  }, {})
+
+  // Add stats to byCollections object
+  await Promise.all(
+    Object.keys(byCollection).map(async (collectionSlug: any) => {
+      const stats = await getCollectionStats(collectionSlug)
+      byCollection[collectionSlug] = { ...byCollection[collectionSlug], stats }
+    }),
+  )
+}
--- pages/api/opensea/[address].ts
+++ pages/api/opensea/[address].ts
@@ -1,49 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-const getAssetsForOwner = async (ownerAddress: string) => {
-  const resp = await fetch(
-    `https://api.opensea.io/api/v1/assets?owner=${ownerAddress}&order_direction=desc&offset=0&limit=50`,
-  )
-  const { assets } = await resp.json()
-  return assets
-}
-
-const getCollectionStats = async (slug: string) => {
-  const resp = await fetch(`https://api.opensea.io/api/v1/collection/${slug}/stats`)
-  const { stats } = await resp.json()
-  return stats
-}
+import { getCollectionsForOwner } from '../../../lib/opensea'
 
 /**
- * Fetches the assets of the given address using the Opensea API.
+ * Fetches the collections of the given address using the Opensea API.
  * https://api.opensea.io/api/v1/assets
  */
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { address: ownerAddress } = req.query
   if (typeof ownerAddress !== 'string') return res.status(400).json({ error: 'ownerAddress must be given' })
-
-  // Get all assets for the given address
-  const assets = await getAssetsForOwner(ownerAddress)
-
-  // Group all assets by collection slug
-  const byCollection = assets.reduce((acc: any, asset: any) => {
-    const slug = asset.collection.slug
-    console.log({ slug })
-    if (acc[slug]) {
-      acc[slug].assets.push(asset)
-    } else {
-      acc[slug] = { ...asset.collection, assets: [asset] }
-    }
-    return acc
-  }, {})
-
-  // Add stats to byCollections object
-  await Promise.all(
-    Object.keys(byCollection).map(async (collectionSlug: any) => {
-      const stats = await getCollectionStats(collectionSlug)
-      byCollection[collectionSlug] = { ...byCollection[collectionSlug], stats }
-    }),
-  )
-
-  res.status(200).json(byCollection)
+  const collections = await getCollectionsForOwner(ownerAddress)
+  res.status(200).json({ collections })
 }

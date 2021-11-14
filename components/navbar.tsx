f307a85ed18b48ef62f3ea036143981ad63e6e96
Jonathan Cai <caimjonathan@gmail.com>
2021-11-13 21:36:01 -0500

    feat: mobile navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -51,70 +51,79 @@ const Navbar = ({ displaySearchbar = true, displayConnectButton = true, customSt
   const formattedETH = parseFloat(formatUnits(balance)).toFixed(2)
 
   return (
-    <nav className="flex justify-between w-full px-4 py-8 items-center space-x-4">
-      {/* Logo */}
+    <>
+      <nav className="w-full px-4 pt-8 md:py-8 items-center">
+        <div className="flex justify-between">
+          {/* Logo */}
 
-      <Link href="/">
-        <div className="ml-1">
-          <Emoji className="text-4xl cursor-pointer" label="logo" symbol="🦧" />
-        </div>
-      </Link>
+          <Link href="/">
+            <div className="ml-1">
+              <Emoji className="text-4xl cursor-pointer" label="logo" symbol="🦧" />
+            </div>
+          </Link>
 
-      <ConnectModal setIsOpen={setModalIsOpen} isOpen={modalIsOpen} />
-      {displaySearchbar !== false && (
-        <div className="w-96">
-          <Searchbar />
-        </div>
-      )}
+          <ConnectModal setIsOpen={setModalIsOpen} isOpen={modalIsOpen} />
+          {displaySearchbar !== false && (
+            <div className="hidden md:block w-96">
+              <Searchbar />
+            </div>
+          )}
 
-      {/* Connect to web3, dark mode toggle */}
-      <div className="flex items-center space-x-2">
-        {status === 'connected' ? (
+          {/* Connect to web3, dark mode toggle */}
           <div className="flex items-center space-x-2">
-            <span
-              className={classnames(
-                'inline-flex items-center px-3 py-0.5 rounded-full text-xs md:text-sm font-medium',
-                {
-                  'bg-indigo-100 text-indigo-800': networkName == 'main',
-                  'bg-yellow-100 text-yellow-800': networkName !== 'main',
-                },
-              )}
-            >
-              <svg
-                className={classnames('-ml-1 mr-1.5 h-2 w-2', {
-                  'text-indigo-400': networkName == 'main',
-                  'text-yellow-400': networkName !== 'main',
-                })}
-              >
-                <svg
-                  className={classnames('-ml-1 mr-1.5 h-2 w-2', {
-                    'text-indigo-400': networkName == 'main',
-                    'text-yellow-400': networkName !== 'main',
-                  })}
-                  fill="currentColor"
-                  viewBox="0 0 8 8"
+            {status === 'connected' ? (
+              <div className="flex items-center space-x-2">
+                <span
+                  className={classnames(
+                    'hidden md:inline-flex items-center px-3 py-0.5 rounded-full text-xs md:text-sm font-medium',
+                    {
+                      'bg-indigo-100 text-indigo-800': networkName == 'main',
+                      'bg-yellow-100 text-yellow-800': networkName !== 'main',
+                    },
+                  )}
                 >
-                  <circle cx={4} cy={4} r={3} />
-                </svg>
-              </svg>
-              {networkName == 'main' ? `Mainnet` : networkName}
-            </span>
-            <AddressPill address={account ? account : ''} ensName={ensName} balance={formattedETH} />
-            <Button
-              type="button"
-              className="inline-flex items-center p-2 rounded-md shadow-sm bg-white text-black border border-solid border-gray-200 hover:bg-gray-100 
+                  <svg
+                    className={classnames('-ml-1 mr-1.5 h-2 w-2', {
+                      'text-indigo-400': networkName == 'main',
+                      'text-yellow-400': networkName !== 'main',
+                    })}
+                  >
+                    <svg
+                      className={classnames('-ml-1 mr-1.5 h-2 w-2', {
+                        'text-indigo-400': networkName == 'main',
+                        'text-yellow-400': networkName !== 'main',
+                      })}
+                      fill="currentColor"
+                      viewBox="0 0 8 8"
+                    >
+                      <circle cx={4} cy={4} r={3} />
+                    </svg>
+                  </svg>
+                  {networkName == 'main' ? `Mainnet` : networkName}
+                </span>
+                <AddressPill address={account ? account : ''} ensName={ensName} balance={formattedETH} />
+                <Button
+                  type="button"
+                  className="inline-flex items-center p-2 rounded-md shadow-sm bg-white text-black border border-solid border-gray-200 hover:bg-gray-100 
               dark:text-gray-100 dark:bg-gray-800 dark:border-gray-700"
-              onClick={handleLogout}
-            >
-              <LogoutIcon className="h-4 w-4 fill-current" />
-            </Button>
+                  onClick={handleLogout}
+                >
+                  <LogoutIcon className="h-4 w-4 fill-current" />
+                </Button>
+              </div>
+            ) : (
+              displayConnectButton !== false && <Button onClick={() => setModalIsOpen(true)}>Connect</Button>
+            )}
+            <DarkModeToggle />
           </div>
-        ) : (
-          displayConnectButton !== false && <Button onClick={() => setModalIsOpen(true)}>Connect</Button>
+        </div>
+        {displaySearchbar !== false && (
+          <div className="block md:hidden w-full my-4">
+            <Searchbar />
+          </div>
         )}
-        <DarkModeToggle />
-      </div>
-    </nav>
+      </nav>
+    </>
   )
 }
 

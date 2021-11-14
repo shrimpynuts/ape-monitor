9fb81a875b6978418b6966412ea8df33a8d2e21d
Jonathan Cai <caimjonathan@gmail.com>
2021-11-14 18:49:13 -0500

    feat: modularize profile details and add loading state with types

---
+++ components/profileDetails.tsx
@@ -0,0 +1,68 @@
+import Davatar from '@davatar/react'
+
+import { ITotalStats, IAddressData } from '../pages/[address]'
+import { middleEllipses, fixedNumber } from '../lib/util'
+import DeltaDisplay from '../components/deltaDisplay'
+import useWeb3Container from '../hooks/useWeb3User'
+import Tooltip from '../components/tooltip'
+import Spinner from '../components/spinner'
+
+interface IProps {
+  totalStats: ITotalStats
+  addressData: IAddressData
+  loading: boolean
+}
+
+const ProfileDetails: React.FC<IProps> = ({
+  loading,
+  totalStats: { oneDayChange, value, assetsOwned, costBasis },
+  addressData: { ensDomain, address },
+}) => {
+  const { ethPrice } = useWeb3Container.useContainer()
+
+  return (
+    <div
+      className="flex flex-col px-2 py-4 space-y-2 md:space-y-0 mx-4 shadow sm:rounded-lg bg-gray-100 dark:bg-gray-850 text-gray-500 dark:text-gray-100
+      md:flex-row md:items-center md:divide-x divide-gray-200 dark:divide-gray-700"
+    >
+      {/* Avatar and name */}
+      <div className="flex text-lg font-bold px-1 md:px-4 space-x-2 items-center border-b md:border-b-0 md:border-r  py-2 md:py-0">
+        <Davatar
+          size={20}
+          address={address}
+          generatedAvatarType="jazzicon" // optional, 'jazzicon' or 'blockies'
+        />
+        <h4 className="text-xl lowercase ">{ensDomain ? ensDomain : middleEllipses(address, 4, 6, 4)}</h4>
+      </div>
+
+      {!loading && (
+        <>
+          {/* # of NFTs */}
+          <h4 className="text-sm px-1 md:px-4 ">{assetsOwned} NFTs</h4>
+          {/* Total Value */}
+          <h4 className="text-sm px-1 space-between md:px-4 relative flex space-x-2 items-center ">
+            <div className="flex-none">
+              <Tooltip text="Based on floor prices, discounting rarity" />
+            </div>
+            <div className="flex space-x-2 items-center flex-grow">
+              Total Value: {fixedNumber(value)}Ξ &nbsp; {ethPrice && <div>(${fixedNumber(ethPrice * value, 0)}) </div>}
+              <DeltaDisplay delta={oneDayChange} denomination="%" />
+            </div>
+          </h4>
+          {/* Total Cost Basis */}
+          <h4 className="text-sm px-1 space-between md:px-4 relative flex space-x-2 items-center ">
+            <Tooltip text="May be inaccurate due to mint costs" />
+            <div className="flex space-x-2 items-center flex-grow">
+              <span>Total Cost Basis: {fixedNumber(costBasis)}Ξ &nbsp; </span>
+              {ethPrice && <div>(${fixedNumber(costBasis * ethPrice, 0)}) </div>}
+            </div>
+          </h4>
+          {/* Price of Ethereum (if available) */}
+          {ethPrice && <h4 className="hidden md:block text-sm px-1 md:px-4">ETH Price: ${ethPrice}</h4>}
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ProfileDetails
--- pages/[address].tsx
+++ pages/[address].tsx
@@ -3,37 +3,47 @@ import { GetServerSideProps } from 'next'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Toaster } from 'react-hot-toast'
-import Davatar from '@davatar/react'
 
 import Navbar from '../components/navbar'
-import { middleEllipses, fixedNumber, getOpenseaData, getNetworkAddress, isENSDomain } from '../lib/util'
+import { middleEllipses, getOpenseaData, getNetworkAddress, isENSDomain } from '../lib/util'
 import CollectionsTable from '../components/collectionsTable'
-import DeltaDisplay from '../components/deltaDisplay'
-import Tooltip from '../components/tooltip'
-import useWeb3Container from '../hooks/useWeb3User'
+import ProfileDetails from '../components/profileDetails'
 
-const AddressPage: NextPage = ({
-  // collections,
-  address,
-  ensDomain,
-}: any) => {
-  const { ethPrice } = useWeb3Container.useContainer()
+interface ICollection {}
 
-  const [
-    {
-      collections,
-      totalStats: { oneDayChange, value, assetsOwned, costBasis },
-    },
-    setOpenseaData,
-  ] = useState({
+export interface ITotalStats {
+  oneDayChange: number
+  value: number
+  assetsOwned: number
+  costBasis: number
+}
+interface IOpenseaDataState {
+  collections: ICollection[]
+  totalStats: ITotalStats
+}
+
+export interface IAddressData {
+  address: string
+  ensDomain?: string
+}
+
+const AddressPage: NextPage<IAddressData> = (addressData) => {
+  const [{ collections, totalStats }, setOpenseaData] = useState<IOpenseaDataState>({
     collections: [],
     totalStats: { oneDayChange: 0, value: 0, assetsOwned: 0, costBasis: 0 },
   })
+  const { address, ensDomain } = addressData
+  const [loading, setLoading] = useState(true)
 
+  /**
+   * Fetches data from opensea at the /api/opensea endpoint and updates state client-side
+   */
   useEffect(() => {
     const initialFetch = async () => {
       setOpenseaData(await getOpenseaData(address))
+      setLoading(false)
     }
+    setLoading(true)
     initialFetch()
   }, [address])
 
@@ -63,42 +73,7 @@ const AddressPage: NextPage = ({
       <Navbar />
 
       {/* Display profile details */}
-      <div
-        className="flex flex-col px-2 py-4 space-y-2 md:space-y-0 mx-4 shadow sm:rounded-lg bg-gray-100 dark:bg-gray-850 text-gray-500 dark:text-gray-100
-      md:flex-row md:items-center md:divide-x divide-gray-200 dark:divide-gray-700"
-      >
-        {/* Avatar and name */}
-        <div className="flex text-lg font-bold px-1 md:px-4 space-x-2 items-center border-b md:border-b-0 md:border-r  py-2 md:py-0">
-          <Davatar
-            size={20}
-            address={address}
-            generatedAvatarType="jazzicon" // optional, 'jazzicon' or 'blockies'
-          />
-          <h4 className="text-xl lowercase ">{ensDomain ? ensDomain : middleEllipses(address, 4, 6, 4)}</h4>
-        </div>
-        {/* # of NFTs */}
-        <h4 className="text-sm px-1 md:px-4 ">{assetsOwned} NFTs</h4>
-        {/* Total Value */}
-        <h4 className="text-sm px-1 space-between md:px-4 relative flex space-x-2 items-center ">
-          <div className="flex-none">
-            <Tooltip text="Based on floor prices, discounting rarity" />
-          </div>
-          <div className="flex space-x-2 items-center flex-grow">
-            Total Value: {fixedNumber(value)}Ξ &nbsp; {ethPrice && <div>(${fixedNumber(ethPrice * value, 0)}) </div>}
-            <DeltaDisplay delta={oneDayChange} denomination="%" />
-          </div>
-        </h4>
-        {/* Total Cost Basis */}
-        <h4 className="text-sm px-1 space-between md:px-4 relative flex space-x-2 items-center ">
-          <Tooltip text="May be inaccurate due to mint costs" />
-          <div className="flex space-x-2 items-center flex-grow">
-            <span>Total Cost Basis: {fixedNumber(costBasis)}Ξ &nbsp; </span>
-            {ethPrice && <div>(${fixedNumber(costBasis * ethPrice, 0)}) </div>}
-          </div>
-        </h4>
-        {/* Price of Ethereum (if available) */}
-        {ethPrice && <h4 className="hidden md:block text-sm px-1 md:px-4">ETH Price: ${ethPrice}</h4>}
-      </div>
+      <ProfileDetails loading={loading} totalStats={totalStats} addressData={addressData} />
 
       {/* Display collections data */}
       <div className="flex flex-col flex-wrap space-y-2 mt-4 mx-4">
@@ -108,15 +83,22 @@ const AddressPage: NextPage = ({
   )
 }
 
+/**
+ * Error in case getServerSideProps cannot resolve ENS domain or bad address given
+ */
 const error = () => {
   return { props: { address: 'not found', ensDomain: 'not found' } }
 }
 
+/**
+ * Resolves the ENS domain if given, and passes the domain and associated address to the page
+ */
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { params } = context
   const address = params?.address
   if (typeof address !== 'string') return error()
 
+  // Fetches the network address (starts with "0x") if given an ENS domain
   const networkAddress = isENSDomain(address) ? await getNetworkAddress(address) : address
   if (!networkAddress) return error()
 

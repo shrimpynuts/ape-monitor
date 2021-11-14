79b3309c12ff06c64580a616e983a6df7b540782
Jonathan Cai <caimjonathan@gmail.com>
2021-11-13 19:46:38 -0500

    feat: tiny styling

--- pages/[address].tsx
+++ pages/[address].tsx
@@ -31,7 +31,7 @@ const AddressPage: NextPage = ({
       <Navbar />
 
       {/* Display profile details */}
-      <div className="flex w-full flex-col px-6 py-4 space-y-4 mx-4 shadow sm:rounded-lg bg-gray-100 dark:bg-gray-850 text-gray-500 dark:text-gray-100">
+      <div className="flex flex-col px-6 py-4 space-y-4 mx-4 shadow sm:rounded-lg bg-gray-100 dark:bg-gray-850 text-gray-500 dark:text-gray-100">
         <div className="flex flex-col md:flex-row md:items-center md:divide-x divide-gray-200 dark:divide-gray-700">
           <div className="flex text-lg px-4 space-x-2 items-center">
             <Davatar

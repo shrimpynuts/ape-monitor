9ea2a119e5fc0b1b3f23a46604c170defd9d7716
Jonathan Cai <caimjonathan@gmail.com>
2021-11-15 02:40:25 -0500

    feat: change leaderboard to be descending

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -2,21 +2,21 @@ import { gql } from '@apollo/client'
 
 export const GET_LEADERBOARD = gql`
   query GetLeaderboard {
-    totalAssetCount: users(order_by: { totalAssetCount: asc }, limit: 5) {
+    totalAssetCount: users(order_by: { totalAssetCount: desc }, limit: 5) {
       ensDomain
       address
       totalAssetCount
       totalCostBasis
       totalValue
     }
-    totalValue: users(order_by: { totalValue: asc }, limit: 5) {
+    totalValue: users(order_by: { totalValue: desc }, limit: 5) {
       ensDomain
       address
       totalAssetCount
       totalCostBasis
       totalValue
     }
-    totalCostBasis: users(order_by: { totalCostBasis: asc }, limit: 5) {
+    totalCostBasis: users(order_by: { totalCostBasis: desc }, limit: 5) {
       ensDomain
       address
       totalAssetCount

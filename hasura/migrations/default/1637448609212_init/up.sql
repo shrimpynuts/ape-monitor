2ef11bc11a82f7213fd22461461c4adcae2f29cb
Faraaz Nishtar <faraaznishtar@gmail.com>
2021-11-20 17:55:12 -0500

    chore: rename dir

---
+++ hasura/migrations/default/1637448609212_init/up.sql
@@ -0,0 +1,28 @@
+SET check_function_bodies = false;
+CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$;
+CREATE TABLE public.users (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    address text NOT NULL,
+    "ensDomain" text,
+    "totalAssetCount" integer,
+    "totalValue" double precision,
+    "totalCostBasis" double precision
+);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_address_key UNIQUE (address);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_users_updated_at ON public.users IS 'trigger to set value of column "updated_at" to current timestamp on row update';

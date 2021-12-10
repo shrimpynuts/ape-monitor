5fa099676f15f2046742abef59295ea03f6edb6a
Jonathan Cai <caimjonathan@gmail.com>
2021-12-10 18:21:18 -0500

    feat: spinning banana loader

--- components/util/spinner.tsx
+++ components/util/spinner.tsx
@@ -1,21 +1,22 @@
+import Emoji from './emoji'
+
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center">
       <div
         className="
-        border-2 border-t-2 border-gray-800
         loader
         ease-linear
         rounded-full
-        h-4
-        w-4
       "
-      />
+      >
+        <Emoji className="cursor-pointer" label="logo" symbol="🍌" />
+      </div>
       <style jsx>
         {`
           .loader {
             border-top-color: white;
-            animation: spinner 0.6s linear infinite;
+            animation: spinner 1.4s linear infinite;
           }
 
           @keyframes spinner {
@@ -33,3 +34,40 @@ const Spinner = () => {
 }
 
 export default Spinner
+
+/**
+ * This is the original spinner component (not the spinning banana)
+ */
+// const Spinner = () => {
+//   return (
+//     <div className="flex justify-center items-center">
+//       <div
+//         className="
+//         border-2 border-t-2 border-gray-800
+//         loader
+//         ease-linear
+//         rounded-full
+//         h-4
+//         w-4
+//       "
+//       />
+//       <style jsx>
+//         {`
+//           .loader {
+//             border-top-color: white;
+//             animation: spinner 0.6s linear infinite;
+//           }
+
+//           @keyframes spinner {
+//             0% {
+//               transform: rotate(0deg);
+//             }
+//             100% {
+//               transform: rotate(360deg);
+//             }
+//           }
+//         `}
+//       </style>
+//     </div>
+//   )
+// }

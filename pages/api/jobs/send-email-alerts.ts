c6179505d06e26fda72365040d9a12a79c56a4e7
Jonathan Cai <caimjonathan@gmail.com>
2022-02-12 05:57:50 -0500

    refactor: log key

--- pages/api/jobs/send-email-alerts.ts
+++ pages/api/jobs/send-email-alerts.ts
@@ -96,6 +96,8 @@ async function getUsers(): Promise<IUser[]> {
 const request = async (req: NextApiRequest, res: NextApiResponse) => {
   const { key } = req.query
 
+  console.log({ key })
+
   // Verify the key for the request
   if (key !== process.env.SEND_EMAIL_KEY) {
     const error = 'Bad request: missing correct key parameter'

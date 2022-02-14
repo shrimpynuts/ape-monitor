69a10596dde0ed225dc4c44c31f263d0f81ef136
Jonathan Cai <caimjonathan@gmail.com>
2022-02-13 22:20:58 -0500

    feat: cleanup test-email-alerts page

--- pages/test-email-alerts.tsx
+++ pages/test-email-alerts.tsx
@@ -4,26 +4,13 @@ import toast, { Toaster } from 'react-hot-toast'
 
 import CollectionsUpdateEmail from '../components/email/template'
 import { collectionsWithAssets } from '../mock/collections-with-assets.json'
-import { addressIsAdmin } from '../lib/util'
+import { totalVolume, marketCap } from '../mock/top-collections.json'
 import Navbar from '../components/layout/navbar'
-import useWeb3Container from '../hooks/useWeb3User'
 import Button from '../components/util/button'
 
 const TestPage: NextPage = () => {
-  const { wallet } = useWeb3Container.useContainer()
-  const isAdmin = wallet.account ? addressIsAdmin(wallet.account) : false
-
   const onSendEmail = () => {
     toast('Not using this page anymore, use the direct URL.')
-    // const endpoint = `${getServer()}/api/jobs/send-email-alerts?key=${process.env.SEND_EMAIL_KEY}`
-    // fetch(endpoint)
-    //   .then((resp) => JSON.stringify(resp.json()))
-    //   .then(() => toast.success('Sent email!'))
-    //   .catch(toast.error)
-  }
-  const isConnected = wallet.isConnected()
-  const onConnectClick = () => {
-    wallet.connect('injected')
   }
 
   return (
@@ -33,35 +20,23 @@ const TestPage: NextPage = () => {
         <title>Testing Email Alerts</title>
       </Head>
       <Toaster />
-      {!isConnected && !isAdmin && (
-        <div className="flex flex-col items-center w-full md:mx-auto md:w-96 space-y-4 mt-64">
-          <h1 className="text-2xl ">Testing Email Alerts Panel</h1>
-          <Button onClick={onConnectClick}>Connect to Wallet</Button>
+      <div className="flex flex-col space-y-4 mt-4">
+        <div className="flex shadow border p-8">
+          <Button onClick={onSendEmail}>Send Email</Button>
         </div>
-      )}
-      {isConnected && !isAdmin && (
-        <div className="flex flex-col items-center w-full md:mx-auto md:w-96 space-y-4 mt-64">
-          <h1 className="text ">You are not connected to an admin address.</h1>
+        <div className="shadow border p-8">
+          <CollectionsUpdateEmail
+            title="test"
+            // @ts-ignore
+            topCollectionsByTotalVolume={totalVolume}
+            // @ts-ignore
+            topCollectionsByOneDayVolume={marketCap}
+            collectionsWithAssets={collectionsWithAssets}
+            address={'0xd6CB70a88bB0D8fB1be377bD3E48e603528AdB54'}
+          />
+          {/* <WithDebug debug title="test" collectionsWithAssets={collectionsWithAssets} /> */}
         </div>
-      )}
-
-      {isAdmin && (
-        <div className="flex flex-col space-y-4 mt-4">
-          <div className="flex shadow border p-8">
-            <Button onClick={onSendEmail}>Send Email</Button>
-          </div>
-          <div className="shadow border p-8">
-            <CollectionsUpdateEmail
-              title="test"
-              topCollectionsByTotalVolume={[]}
-              topCollectionsByOneDayVolume={[]}
-              collectionsWithAssets={collectionsWithAssets}
-              address={'0xd6CB70a88bB0D8fB1be377bD3E48e603528AdB54'}
-            />
-            {/* <WithDebug debug title="test" collectionsWithAssets={collectionsWithAssets} /> */}
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   )
 }
